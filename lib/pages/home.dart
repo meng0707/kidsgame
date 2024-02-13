4123635f0f62c33ae0b901c71e7696a965113f54
meng0707 <652021045@tsu.ac.th>
2024-02-13 15:34:59 +0700

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,136 @@
+import 'dart:math';
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final Map<String, bool> score = {};
+  final Map<String, Color> choices = {
+    '🍎': Colors.red,
+    '🥒': Colors.green,
+    '🔵': Colors.blue,
+    '🍍': Colors.yellow,
+    '🍊': Colors.orange,
+    '🍇': Colors.purple,
+    '🥥': Colors.brown,
+  };
+  int index = 0;
+  final play = AudioPlayer();
+  Timer? timer; // เพิ่มตัวแปร Timer
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Score: ${score.values.where((element) => element).length}'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: choices.keys.map((element) {
+              return Expanded(
+                child: Draggable<String>(
+                  data: element,
+                  child: Movable(element),
+                  feedback: Movable(element),
+                  childWhenDragging: Movable('🐰'),
+                ),
+              );
+            }).toList(),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: choices.keys.map((element) {
+              return buildTarget(element);
+            }).toList()
+              ..shuffle(Random(index)),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          setState(() {
+            score.clear();
+            index++;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget buildTarget(emoji) {
+    return DragTarget<String>(
+      builder: (context, incoming, rejects) {
+        if (score[emoji] == true) {
+          return Container(
+            color: Colors.white,
+            child: Text('Congratulations'),
+            alignment: Alignment.center,
+            height: 80,
+            width: 200,
+          );
+        } else {
+          return Container(
+            color: choices[emoji],
+            height: 80,
+            width: 200,
+          );
+        }
+      },
+      onWillAccept: (data) => data == emoji,
+      onAccept: (data) {
+        setState(() {
+          score[emoji] = true;
+          play.play(AssetSource('clap.mp3'));
+        });
+      },
+      onLeave: (data) {
+        // เริ่มต้นนับถอยหลังทุกครั้งที่ผู้ใช้ลากตัวเลือกออกจากตำแหน่ง
+        timer?.cancel(); // ยกเลิก Timer ที่มีอยู่ (ถ้ามี)
+        timer = Timer(Duration(seconds: 3), () {
+          // หลังจาก 3 วินาทีจะทำงาน
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('คำตอบที่เลือก "$data" ผิด!'),
+              duration: Duration(seconds: 2),
+            ),
+          );
+        });
+      },
+    );
+  }
+}
+
+class Movable extends StatelessWidget {
+  final String emoji;
+  Movable(this.emoji);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        alignment: Alignment.center,
+        height: 50,
+        padding: EdgeInsets.all(15),
+        child: Text(
+          emoji,
+          style: TextStyle(color: Colors.black, fontSize: 60),
+        ),
+      ),
+    );
+  }
+}
